2ddcb7b885298e87340b629c30562eefc5041e7e
Sophia <snlogarta@gmail.com>
2026-05-01 17:56:19 +0800

    Added the initial classes for the game logic with code for Serialization of Player class

--- src/main/java/data/Student.java
+++ src/main/java/data/Student.java
@@ -3,5 +3,11 @@ package data;
 public class Student {
     private StudentState state;
 
-    public
+    public void setState(StudentState state){
+        this.state = state;
+    }
+
+    public void request(){
+        state.handleRequest();
+    }
 }
